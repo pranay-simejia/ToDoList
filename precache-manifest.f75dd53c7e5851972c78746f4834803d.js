138931aab62c3ce9cd4408b39a89aa2fcc90db53
pranay-simejia <simejiapranay@gmail.com>
2020-09-19 02:10:08 +0530

    Updates

---
+++ precache-manifest.f75dd53c7e5851972c78746f4834803d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef69cdb14a0dc0a7e062c67604f078d",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "0cfd2bca9cbfd0421a7d",
+    "url": "/ToDoList/static/css/main.ab3304d8.chunk.css"
+  },
+  {
+    "revision": "528fdd3cdb65c2bf6e78",
+    "url": "/ToDoList/static/js/2.3a15a693.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList/static/js/2.3a15a693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cfd2bca9cbfd0421a7d",
+    "url": "/ToDoList/static/js/main.6be89548.chunk.js"
+  },
+  {
+    "revision": "af53d6e5829eda6d6164",
+    "url": "/ToDoList/static/js/runtime-main.5215122a.js"
+  }
+]);
